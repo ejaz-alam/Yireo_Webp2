99cd539aa07f792909803630f5ef32c55d2f09fa
Ejaz Alam <ejazalam518@gmail.com>
2025-03-14 10:16:34 +0000

    add class once the body element is loaded

--- view/frontend/web/js/add-webp-class-to-body.js
+++ view/frontend/web/js/add-webp-class-to-body.js
@@ -2,9 +2,13 @@ define([
     'jquery',
     './has-webp'
 ], function($, hasWebP) {
-    if (hasWebP()) {
-        $('body').addClass('webp');
-    } else {
-        $('body').addClass('no-webp');
+    document.onreadystatechange = function () {
+        if (document.readyState == "interactive") {
+            if (hasWebP()) {
+                $('body').addClass('webp');
+            } else {
+                $('body').addClass('no-webp');
+            }
+        }
     }
 });
